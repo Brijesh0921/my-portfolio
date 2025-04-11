c3dfa08994f1991b6ec94e402da1df2ce92cc11a
BritZz <142307517+Brijesh0921@users.noreply.github.com>
2025-04-11 15:54:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,23 @@ function addRecommendation() {
 
     // Create a new 'recommendation' element and set it's value to the user's message
     var element = document.createElement("div");
-    element.setAttribute("class","recommendation");
-    element.innerHTML = "\<span\>&#8220;\</span\>" + recommendation.value + "\<span\>&#8221;\</span\>";
+    element.setAttribute("class", "recommendation");
+    element.innerHTML =
+      "<span>&#8220;</span>" + recommendation.value + "<span>&#8221;</span>";
     // Add this element to the end of the list of recommendations
-    document.getElementById("all_recommendations").appendChild(element); 
-    
+    document.getElementById("all_recommendations").appendChild(element);
+
     // Reset the value of the textarea
     recommendation.value = "";
+
+    showPopup(true);
   }
 }
 
 function showPopup(bool) {
   if (bool) {
-    document.getElementById('popup').style.visibility = 'visible'
+    document.getElementById("popup").style.visibility = "visible";
   } else {
-    document.getElementById('popup').style.visibility = 'hidden'
+    document.getElementById("popup").style.visibility = "hidden";
   }
 }
